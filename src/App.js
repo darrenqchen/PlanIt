72735eccb8078e70e9a069ba5cf2301b04e54289
ben-mallett <bencmallett@outlook.com>
2022-10-10 01:47:16 -0400

    added under construction, removed title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import Stack from '@mui/material/Stack';
+import { Typography } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div
-      className="App"
       style={{
-        backgroundImage: 'url(/landingpage.jpg)',
+        backgroundImage: 'url(/under-construction.png)',
         width: '100vw',
         height: '100vh'
       }}
-    ></div>
+    >
+      <Stack>
+        <Typography>planit</Typography>
+      </Stack>
+    </div>
   );
 }
 
