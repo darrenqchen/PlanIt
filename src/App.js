425f0c2f26c48d7d9d781554ad08143311aab53d
ben-mallett <bencmallett@outlook.com>
2022-10-14 16:31:58 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { ThemeProvider } from '@mui/material';
 
 import './App.css';
 import LandingPage from './components/LandingPage';
-import { theme } from './config/Theme'
-
+import { theme } from './config/Theme';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <LandingPage></LandingPage>
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} exact />
+          {/* <Route path="/plan" component={PlanPage} /> */}
+          <Route element={Error} />
+        </Routes>
+      </main>
     </ThemeProvider>
   );
 }
