5486b590d586823cd1b2d6c26269dded1a1c13ac
ben-mallett <bencmallett@outlook.com>
2022-10-14 15:57:18 -0400

    basic landing page functionality

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,34 +1,131 @@
-import React, { useState, useEffect } from 'react'
-import { Stack } from '@mui/material'
-import Typography from '@mui/material/Typography'
+import React, { useState, useEffect } from 'react';
+import {
+  IconButton,
+  Stack,
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  Divider,
+  Grid
+} from '@mui/material';
+import Typography from '@mui/material/Typography';
+import ModeOfTravelIcon from '@mui/icons-material/ModeOfTravel';
+import { borderRadius } from '@mui/system';
 
+import PlanitLogoFilled from './PlanitLogoFilled';
+import PlanitLogoOutlined from './PlanitLogoOutlined';
 
 const LandingPage = () => {
-    const [activeBackground, setActiveBackground] = useState("")
-    const potentialBackgrounds = ['/chichenitza.jpg', '/chichenitza.jpg', '/chichenitza.jpg']
+  const [activeBackground, setActiveBackground] = useState({});
+  const [firstRender, setFirstRender] = useState(true);
+  const potentialBackgrounds = [
+    {
+      url: '/chichenitza.jpg',
+      name: 'Chichen Itza',
+      location: 'Mexico',
+      carbonCost: '140kgCE',
+      description: 'a lovely place to visit'
+    },
+    {
+      url: '/grandcanyon.jpg',
+      name: 'Grand Canyon',
+      location: 'USA',
+      carbonCost: '140kgCE',
+      description: 'beautiful and awe inspiring'
+    },
+    {
+      url: '/iceland.jpg',
+      name: 'Iceland Beach',
+      carbonCost: '140kgCE',
+      location: 'Iceland',
 
-    useEffect(() => {
-        setActiveBackground(potentialBackgrounds[Math.floor(Math.random() * potentialBackgrounds.length)])
-    })
+      description: "don't freeze when you come here"
+    }
+  ];
 
-    return (
-        <div
-            style={
-                {
-                    backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), 
-                url(${activeBackground})`,
-                
-                    backgroundSize: "cover",
-                    height: "100vh"
-                }
-            }
-        >
-            <Stack paddingTop={25} paddingLeft={30}>
-                <Typography variant="h1" color="text.secondary">planit</Typography>
-            </Stack>
-        </div>
-    )
-}
+  useEffect(() => {
+    if (firstRender) {
+      setActiveBackground(
+        potentialBackgrounds[
+          Math.floor(Math.random() * potentialBackgrounds.length)
+        ]
+      );
+    }
+    setFirstRender(false);
+  });
 
+  return (
+    <div
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05)), 
+                url(${activeBackground.url})`,
+
+        backgroundSize: 'cover',
+        height: '100vh'
+      }}
+    >
+      <Stack
+        paddingTop={'8%'}
+        paddingLeft={'10%'}
+        direction="row"
+        spacing={'45%'}
+      >
+        <PlanitLogoFilled />
+        <Stack alignItems="start" spacing="140%">
+          <Card
+            sx={{
+              minWidth: 200,
+              backdropFilter: 'blur(14px)',
+              backgroundColor: 'rgba(255, 255, 255, 0.01)'
+            }}
+          >
+            <CardContent>
+              <Stack direction="row" spacing={2}>
+                <Typography variant="h5" color="text.secondary" gutterBottom>
+                  {activeBackground.name}
+                </Typography>
+                <Divider
+                  orientation="vertical"
+                  variant="middle"
+                  color="primary.dark"
+                  flexItem
+                />
+                <Typography variant="h5" color="text.secondary" gutterBottom>
+                  {activeBackground.location}
+                </Typography>
+              </Stack>
+              <Typography variant="h6" color="text.secondary" component="div">
+                {activeBackground.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <IconButton>
+                <ModeOfTravelIcon color="primary.dark" />
+              </IconButton>
+              <Typography paddingLeft={1} color="text.secondary">
+                Plan a trip here
+              </Typography>
+            </CardActions>
+          </Card>
+          <div>
+            <Button
+              variant="outlined"
+              sx={{
+                minWidth: 200,
+                backdropFilter: 'blur(1px)',
+                backgroundColor: 'rgba(255, 255, 255, 0.01)',
+                borderRadius: '20px'
+              }}
+              endIcon={<div></div>}
+            >
+              {'Plan your trip'}
+            </Button>
+          </div>
+        </Stack>
+      </Stack>
+    </div>
+  );
+};
 
 export default LandingPage;
